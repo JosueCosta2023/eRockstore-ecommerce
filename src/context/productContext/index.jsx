21670c8eebfdddeebf2223ad6aa5ba83d76b52dc
Josué Ocanha Costa <contato_josuecosta@hotmail.com>
2024-05-29 22:28:08 -0400

    update: products games

--- src/context/productContext/index.jsx
+++ src/context/productContext/index.jsx
@@ -7,40 +7,7 @@ export const ProductContext = createContext();
 
 
 export const ProductProvider = ({ children }) => {
-  // const [cartItems, setCartItems] = useState([])
 
-  // const addToCart = (product) => {
-  //     const existingProduct = cartItems.find(item => item.id === product.id);
-  //     if (existingProduct) {
-  //       setCartItems(cartItems.map(item => 
-  //         item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
-  //       ));
-  //     } else {
-  //       setCartItems([...cartItems, { ...product, quantity: 1 }]);
-  //     }
-
-  // }
-
-
-  // const removeFromCart = (productId) => {
-  //     const existingProduct = cartItems.find(item => item.id === productId);
-  //     if (existingProduct.quantity === 1) {
-  //       setCartItems(cartItems.filter(item => item.id !== productId));
-  //     } else {
-  //       setCartItems(cartItems.map(item => 
-  //         item.id === productId ? { ...item, quantity: item.quantity - item.quantity  } : item
-  //       ));
-  //     }
-  // }
-
-  // const updateQuantity = (productId, quantity) => {
-  //   setCartItems(
-  //     cartItems.map(item => 
-  //       item.id === productId ? {...item, quantity: quantity} : item
-  //     )
-  //   )
-  //   console.log(cartItems)
-  // }
   const [products, setProducts] = useState([])
   const [cart, setCart] = useState([])
   const [valueCart, setValueCart] = useState([])
@@ -173,8 +140,7 @@ export const ProductProvider = ({ children }) => {
 
     // Salvar PDF
     const DataAtual = Date.now();
-    const Hoje = Math.floor(DataAtual * (365.25 * 24 * 60 * 60 * 1000))
-    documento.save(`${Hoje}-orcamento.pdf`);
+    documento.save(`${DataAtual}-orcamento.pdf`);
 
     clearCart({cart})
   
